88d7644ea6d88d0928b0aec6d83b9b04c85c7fac
Randy Olson <rso@randalolson.com>
2017-04-06 13:02:22 -0400

    Fix distarray_missing parallelization
    The function should not use joblib if n_jobs==1

--- skrebate/relieff.py
+++ skrebate/relieff.py
@@ -282,8 +282,11 @@ class ReliefF(BaseEstimator):
         for i in range(self._datalen):
             cindices.append(np.where(np.isnan(xc[i]))[0])
             dindices.append(np.where(np.isnan(xd[i]))[0])
-    
-        dist_array = Parallel(n_jobs=self.n_jobs)(delayed(self._get_row_missing)(xc, xd, cdiffs, index, cindices, dindices) for index in range(self._datalen))
+
+        if n_jobs != 1:
+            dist_array = Parallel(n_jobs=self.n_jobs)(delayed(self._get_row_missing)(xc, xd, cdiffs, index, cindices, dindices) for index in range(self._datalen))
+        else:
+            dist_array = [self._get_row_missing(xc, xd, cdiffs, index, cindices, dindices) for index in range(self._datalen)]
         return np.array(dist_array)
     #==================================================================#
     def _get_row_missing(self, xc, xd, cdiffs, index, cindices, dindices):
