Fixed 'get_row_missing() in scoring_utils.py so that distance array calculation is properly normalized by the number of uniquely missing features in comparing instance pair distances. This way distance is computed agnostically with respect to missing values.  The previous implementation would make instances with more missing values appear closer to one another.

skrebate/scoring_utils.py
@@ -34,6 +34,7 @@ def get_row_missing(xc, xd, cdiffs, index, cindices, dindices): #(Subset of cont
     dinst1 = xd[index] #discrete-valued features for index instance
     can = cindices[index] #Boolean mask locating missing values for continuous features for index instance
     dan = dindices[index] #Boolean mask locating missing values for discrete features for index instance
+    tf = len(cinst1) + len(dinst1) #total number of features. 
     
     #Progressively compare current instance to all others. Excludes comparison with self indexed instance. (Building the distance matrix triangle).
     for j in range(index):
@@ -44,14 +45,14 @@ def get_row_missing(xc, xd, cdiffs, index, cindices, dindices): #(Subset of cont
         # Manage missing values in discrete features
         dbn = dindices[j] #Boolean mask locating missing values for discrete features for compared instance
         idx = np.unique(np.append(dan, dbn)) #indexes where there is at least one missing value in the feature between an instance pair. 
-        #dmc = len(idx) # Number of features excluded from distance calculation due to one or two missing values within instance pair. Used to normalize distance values for comparison.
+        dmc = len(idx) # Number of features excluded from distance calculation due to one or two missing values within instance pair. Used to normalize distance values for comparison.
         d1 = np.delete(dinst1, idx) #delete unique missing features from index instance
         d2 = np.delete(dinst2, idx) #delete unique missing features from compared instance
         
         # Manage missing values in continuous features
         cbn = cindices[j] #Boolean mask locating missing values for continuous features for compared instance
         idx = np.unique(np.append(can, cbn)) #indexes where there is at least one missing value in the feature between an instance pair. 
-        #cmc = len(idx) # Number of features excluded from distance calculation due to one or two missing values within instance pair. Used to normalize distance values for comparison.
+        cmc = len(idx) # Number of features excluded from distance calculation due to one or two missing values within instance pair. Used to normalize distance values for comparison.
         c1 = np.delete(cinst1, idx) #delete unique missing features from index instance
         c2 = np.delete(cinst2, idx) #delete unique missing features from compared instance
         cdf = np.delete(cdiffs, idx) #delete unique missing features from continuous value difference scores
@@ -62,9 +63,9 @@ def get_row_missing(xc, xd, cdiffs, index, cindices, dindices): #(Subset of cont
         # Add continuous feature distance contributions (missing values excluded) - Manhattan distance
         dist += np.sum(np.absolute(np.subtract(c1, c2)) / cdf)  #possible normalization problem here.
         
-        #Normalize distance calculation based on number of missing values bypassed
-        #nmc = self._num_attributes
-        #dist = dist/float(nmc)
+        #Normalize distance calculation based on total number of missing values bypassed in either discrete or continuous features.
+        tnmc = tf - dmc - cmc #Total number of unique missing counted
+        dist = dist/float(tnmc) #Distance normalized by number of features included in distance sum. 
 
         row = np.append(row, dist)
     return row