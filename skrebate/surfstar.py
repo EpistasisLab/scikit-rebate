2e2e2140cefc56be54c2e2305e3366ee98e6c296
Ryan Urbanowicz <ryanurb@upenn.edu>
2018-04-03 14:51:02 -0400

    Included TuRF to init import, and removed unnecessary code from surfstar and multisurfstar for parallelization check.

--- skrebate/surfstar.py
+++ skrebate/surfstar.py
@@ -84,14 +84,9 @@ class SURFstar(SURF):
         NN_near_list = [i[0] for i in NNlist]
         NN_far_list = [i[1] for i in NNlist]
 
-        if self.n_jobs != 1: #Parallelization
-            scores = np.sum(Parallel(n_jobs=self.n_jobs)(delayed(
-                SURFstar_compute_scores)(instance_num, self.attr, nan_entries, self._num_attributes, self.mcmap,
-                                         NN_near, NN_far, self._headers, self._class_type, self._X, self._y, self._labels_std, self.data_type)
-                for instance_num, NN_near, NN_far in zip(range(self._datalen), NN_near_list, NN_far_list)), axis=0)
-        else: #Serial run
-            scores = np.sum([SURFstar_compute_scores(instance_num, self.attr, nan_entries, self._num_attributes, self.mcmap,
-                                                     NN_near, NN_far, self._headers, self._class_type, self._X, self._y, self._labels_std, self.data_type)
-                             for instance_num, NN_near, NN_far in zip(range(self._datalen), NN_near_list, NN_far_list)], axis=0)
+        scores = np.sum(Parallel(n_jobs=self.n_jobs)(delayed(
+            SURFstar_compute_scores)(instance_num, self.attr, nan_entries, self._num_attributes, self.mcmap,
+                                     NN_near, NN_far, self._headers, self._class_type, self._X, self._y, self._labels_std, self.data_type)
+            for instance_num, NN_near, NN_far in zip(range(self._datalen), NN_near_list, NN_far_list)), axis=0)
 
         return np.array(scores)
