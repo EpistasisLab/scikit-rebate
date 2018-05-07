Fixed minor bug in distance array calculation
for continuous features, did not overwrite original data with normalized
values needed for distance array calculation.  This can slightly
(incorrectly) alter the continuous-value feature scores after scoring,
since normalization takes place again during scoring for continuous
values.

skrebate/relieff.py
@@ -318,8 +318,8 @@ class ReliefF(BaseEstimator):
             c_dist = squareform(pdist(pre_normalize(xc), metric='cityblock')) #Cityblock is also known as Manhattan distance
             return np.add(d_dist, c_dist) / self._num_attributes
         else: #continuous features only
-            xc = pre_normalize(xc)
-            return squareform(pdist(xc, metric='cityblock'))
+            #xc = pre_normalize(xc)
+            return squareform(pdist(pre_normalize(xc), metric='cityblock'))
 
     #==================================================================#
     def _dtype_array(self):