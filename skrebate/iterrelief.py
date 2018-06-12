c1fae37c656ac2f0df16534081b45414b26f1e6a
sauravbose <saurav.bose93@gmail.com>
2018-06-12 12:56:11 -0400

    Iterrelief completed

--- skrebate/iterrelief.py
+++ skrebate/iterrelief.py
@@ -31,7 +31,7 @@ class IterRelief(BaseEstimator, TransformerMixin):
     """
 
     def __init__(self, core_algorithm, weight_flag=2, n_features_to_select=2, n_neighbors=100, max_iter=10, discrete_threshold=10, verbose=False, n_jobs=1):
-        """Sets up VLSRelief to perform feature selection.
+        """Sets up IterRelief to perform feature selection.
 
         Parameters
         ----------
@@ -42,10 +42,8 @@ class IterRelief(BaseEstimator, TransformerMixin):
         n_features_to_select: int (default: 10)
             the number of top features (according to the relieff score) to
             retain after feature selection is applied.
-        num_feature_subset: int (default: 40)
-            Number of subsets generated at random
-        size_feature_subset: int (default 5)
-            Number of features in each subset generated
+        max_iter: int (default 10)
+            the maximum number of iterations to be performed.
         discrete_threshold: int (default: 10)
             Value used to determine if a feature is discrete or continuous.
             If the number of unique levels in a feature is > discrete_threshold, then it is
@@ -68,14 +66,11 @@ class IterRelief(BaseEstimator, TransformerMixin):
         self.max_iter = max_iter
 
     #=========================================================================#
-    # headers = list(genetic_data.drop("class",axis=1))
     def fit(self, X, y):
         """
-        Generates `num_feature_subset` sets of features each of size `size_feature_subset`.
-        Thereafter, uses the input `core_algorithm` to determine feature importance scores
-        for each subset. The global feature score is determined by the max score for that feature
-        from all its occurences in the subsets generated. Once the final feature scores are obtained,
-        the top `n_features_to_select` features can be selected.
+        Runs a core relief algorithm to generate feauture importance scores. Then these
+        scores are used to update the distance vector and the scores for the next iteration.
+        The process repeats till a user defined number of iterations has been run.
 
         Parameters
         ----------
@@ -83,17 +78,16 @@ class IterRelief(BaseEstimator, TransformerMixin):
             Training instances to compute the feature importance scores from
         y: array-like {n_samples}
             Training labels
-        headers: array-like {n_features}
-            Feature names
         Returns
         -------
-        Copy of the VLSRelief instance
+        Copy of the IterRelief instance
         """
 
         self.X_mat = X
         self._y = y
         #self.headers = headers
 
+        # Combine IterRelief with specified 'core' Relief-based algorithm
         if self.core_algorithm.lower() == "multisurf":
             core = MultiSURF()
 
@@ -109,26 +103,36 @@ class IterRelief(BaseEstimator, TransformerMixin):
         elif self.core_algorithm.lower() == "relieff":
             core = ReliefF()
 
+        # Determine total number of features
         total_num_features = X.shape[1]
+        # Initialize weights
         weights = np.ones(total_num_features)
 
         iteration = 0
+        weight_history = []
 
-        while (iteration < self.max_iter):
+        # Iterate till max iteration reached or all weights are really tiny
+        while ((iteration < self.max_iter) & (any(w >= 0.1 for w in weights))):
+            # Run Core Relief-based algorithm
             core_fit = core.fit(self.X_mat, self._y, weights, self.weight_flag)
 
             # When all weights become 0, break
             if all(w == 0 for w in core_fit.feature_importances_):
                 break
 
+            # Update weights
             weights = core_fit.feature_importances_
+            # Round negative weights to 0
             weights = [0 if i < 0 else i for i in weights]
+            weight_history.append(weights)
 
             # print('iter', iteration)
             # print('w', weights)
             iteration += 1
 
+        #print('final w', weights)
         self.feature_importances_ = weights
+        self.history = weight_history
         self.top_features_ = np.argsort(self.feature_importances_)[::-1]
         #self.header_top_features_ = [self.headers_model[i] for i in self.top_features_]
 
@@ -157,7 +161,7 @@ class IterRelief(BaseEstimator, TransformerMixin):
 
     #=========================================================================#
 
-    def fit_transform(self, X, y, headers):
+    def fit_transform(self, X, y):
         # def fit_transform(self, X, y):
         """Computes the feature importance scores from the training data, then reduces the feature set down to the top `n_features_to_select` features.
 
@@ -174,5 +178,5 @@ class IterRelief(BaseEstimator, TransformerMixin):
             Reduced feature matrix
 
         """
-        self.fit(X, y, headers)
+        self.fit(X, y)
         return self.transform(X)
