df5944a70b1a6347628f2f24bb3a9a79937e08f9
MichaelYesuyu <42868821+MichaelYesuyu@users.noreply.github.com>
2020-07-02 16:04:27 -0400

    Added ReliefF to the analysis pipeline

--- analysis_pipeline/jobSubmissionMain.py
+++ analysis_pipeline/jobSubmissionMain.py
@@ -7,7 +7,7 @@ import jobSubmission
 '''
 Sample Run Code:
 python jobSubmissionMain.py --data-path /home/robertzh/data --output-path /home/robertzh/outputs --experiment-name run2
-python jobSubmissionMain.py --data-path /Users/robert/Desktop/rebateDatasets --output-path /Users/robert/Desktop/outputs --experiment-name rebate1
+python jobSubmissionMain.py --data-path /Users/yesuyu/Desktop/ReliefMultiset2 --output-path /Users/yesuyu/Desktop/outputs --experiment-name ReliefMultiset2
 
 '''
 
@@ -48,9 +48,9 @@ def main(argv):
         os.mkdir(output_path + '/' + experiment_name + '/logs')
 
     #CONTROL PANEL######################################################################################################
-    #Choose from 'multisurf','vls','iter','turf','vls_iter','vls_turf','multisurf_abs','vls_abs','iter_abs','turf_abs','vls_iter_abs','vls_turf_abs'
+    #Choose from 'multisurf','vls','iter','turf','vls_iter','vls_turf','multisurf_abs','vls_abs','iter_abs','turf_abs','vls_iter_abs','vls_turf_abs','relieff_100nn','relieff_100nn_abs'
 
-    algorithms_to_use = ['multisurf','vls','iter','turf','vls_turf','vls_iter']
+    algorithms_to_use = ['relieff_100nn','relieff_100nn_abs','multisurf','multisurf_abs']
 
     ####################################################################################################################
 
@@ -63,8 +63,8 @@ def main(argv):
                 continue
             for algorithm in algorithms_to_use:
                 outfile = output_path + '/' + experiment_name + '/rawoutputs/' + algorithm + '_' + filename[:-3]
-                #submitLocalJob(algorithm,os.path.join(dirpath, filename),class_label,random_state,outfile)
-                submitClusterJob(algorithm, os.path.join(dirpath, filename), output_path + '/' + experiment_name,class_label, random_state,outfile)
+                submitLocalJob(algorithm,os.path.join(dirpath, filename),class_label,random_state,outfile)
+                #submitClusterJob(algorithm, os.path.join(dirpath, filename), output_path + '/' + experiment_name,class_label, random_state,outfile)
 
 def submitLocalJob(algorithm,datapath,class_label,random_state,outfile):
     jobSubmission.job(algorithm,datapath,class_label,random_state,outfile)
